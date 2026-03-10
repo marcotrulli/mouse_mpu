6dc4ea7c9566d20cda7ea41cd901f6860b085b7f
marcotrulli <131980899+marcotrulli@users.noreply.github.com>
2026-03-10 23:05:51 +0100

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -1,31 +1,18 @@
 import socket
-import json
-from pynput.mouse import Controller
+import struct
+import pyautogui
 
-HOST = "0.0.0.0"  # ascolta tutte le interfacce
 PORT = 5005
 
-mouse = Controller()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server TCP in ascolto su {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connessione da {addr}")
-        buffer = ""
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            buffer += data.decode()
-            while "\n" in buffer:
-                line, buffer = buffer.split("\n", 1)
-                try:
-                    payload = json.loads(line)
-                    dx = payload.get("dx", 0)
-                    dy = payload.get("dy", 0)
-                    mouse.move(dx, dy)
-                except Exception as e:
-                    print("Errore:", e)
+print("Server avviato...")
+
+while True:
+
+    data, addr = sock.recvfrom(1024)
+
+    dx, dy = struct.unpack("bb", data)
+
+    pyautogui.moveRel(dx, dy)

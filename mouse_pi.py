0e49e8f5316c85a236231f54274010e121b033e5
marcotrulli <131980899+marcotrulli@users.noreply.github.com>
2026-03-10 23:28:51 +0100

    Update mouse_pi.py

--- mouse_pi.py
+++ mouse_pi.py
@@ -10,7 +10,7 @@ PORT = 5005
 bus = smbus.SMBus(1)
 
 # sensibilità: più grande → mouse più lento
-SENS = 500  
+SENS = 340  
 
 # soglia minima per eliminare micro-movimenti involontari
 THRESH = 0.05  

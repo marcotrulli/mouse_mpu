57ecf7095521ce53c2e3dba56fc176a4590c14b9
marcotrulli <131980899+marcotrulli@users.noreply.github.com>
2026-03-10 23:20:22 +0100

    Update mouse_pi.py

--- mouse_pi.py
+++ mouse_pi.py
@@ -8,7 +8,10 @@ PC_IP = "192.168.1.179"
 PORT = 5005
 
 bus = smbus.SMBus(1)
-sens=10000
+
+# sensibilità: più grande il numero, meno sensibile
+SENS = 4000  
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # wake up MPU6050
@@ -21,16 +24,13 @@ offset_y = 0
 samples = 200
 
 for _ in range(samples):
-
     data = bus.read_i2c_block_data(MPU_ADDR, 0x43, 4)
 
     gx = (data[0] << 8) | data[1]
     gy = (data[2] << 8) | data[3]
 
-    if gx > 32767:
-        gx -= 65536
-    if gy > 32767:
-        gy -= 65536
+    if gx > 32767: gx -= 65536
+    if gy > 32767: gy -= 65536
 
     offset_x += gx
     offset_y += gy
@@ -41,27 +41,39 @@ offset_x /= samples
 offset_y /= samples
 
 print("Offset calcolati:", offset_x, offset_y)
-
 print("Connessione al PC...")
 
-while True:
+# accumulatori per gestire piccoli delta
+acc_dx = 0.0
+acc_dy = 0.0
 
+while True:
     data = bus.read_i2c_block_data(MPU_ADDR, 0x43, 4)
 
     gx = (data[0] << 8) | data[1]
     gy = (data[2] << 8) | data[3]
 
-    if gx > 32767:
-        gx -= 65536
-    if gy > 32767:
-        gy -= 65536
+    if gx > 32767: gx -= 65536
+    if gy > 32767: gy -= 65536
 
     gx -= offset_x
     gy -= offset_y
 
-    dx = int(gy / sens)    #  /a con più a è grande più la sensibilità del mouse è bassa 
-    dy = int(gx / sens)
+    # calcolo movimento reale con sensibilità
+    dx_f = gy / SENS
+    dy_f = gx / SENS
 
-    packet = struct.pack("bb", dx, dy)
+    # accumulo i piccoli delta
+    acc_dx += dx_f
+    acc_dy += dy_f
 
-    sock.sendto(packet, (PC_IP, PORT))
+    # invio solo quando accumulo almeno 1 pixel
+    move_x = int(acc_dx)
+    move_y = int(acc_dy)
+
+    if move_x != 0 or move_y != 0:
+        packet = struct.pack("bb", move_x, move_y)
+        sock.sendto(packet, (PC_IP, PORT))
+
+        acc_dx -= move_x
+        acc_dy -= move_y

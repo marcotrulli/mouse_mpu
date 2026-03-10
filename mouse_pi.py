cb8f51dc3f2c661a1e43033fabc89cd067a8184d
marcotrulli <131980899+marcotrulli@users.noreply.github.com>
2026-03-10 23:47:01 +0100

    Update mouse_pi.py

--- mouse_pi.py
+++ mouse_pi.py
@@ -3,8 +3,8 @@ import time
 import socket
 from mpu_reader import MPUReader
 
-PC_IP = "192.168.1.179"  # metti l'IP del PC
-PORT  = 5005
+PC_IP = "192.168.1.179"  # cambia con l'IP del tuo PC
+PORT = 5005
 SCALE_X = 500
 SCALE_Y = 500
 UPDATE_RATE = 0.01  # 100 Hz
@@ -24,13 +24,13 @@ try:
 
         data = sensor.read_filtered(dt)
 
-        # Applica scaling e soglia minima
+        # Scaling e soglia minima
         dx = data["dx"] * SCALE_X
         dy = data["dy"] * SCALE_Y
         if abs(dx) < 0.5: dx = 0
         if abs(dy) < 0.5: dy = 0
 
-        # Prepara payload con dx, dy e valori raw per debug
+        # Prepara payload con solo 8 valori: dx,dy + gx,gy,gz + ax,ay,az
         payload = "{dx},{dy},{gx},{gy},{gz},{ax},{ay},{az}".format(**data)
         sock.send(payload.encode())
 
